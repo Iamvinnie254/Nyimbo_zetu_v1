53b11aca17ac7b46ac58c717707cd56c31e22bc6
Awafula <wafulaabel610@gmail.com>
2026-01-10 23:35:22 +0300

    Add GraphQL Config files Add Type, Queries, Mutation and Resolver for users

--- nyimbo_zetu_backend/app/Models/User.php
+++ nyimbo_zetu_backend/app/Models/User.php
@@ -18,14 +18,13 @@ class User extends Authenticatable
      * @var list<string>
      */
     protected $fillable = [
-        'name',
         'email',
-        'password',
         'first_name',
         'last_name',
-        'image',
         'phone_number',
         'address',
+        'password',
+        'image',
     ];
 
     /**
